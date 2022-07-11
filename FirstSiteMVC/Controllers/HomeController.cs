f0c7372bcf4227602d79919d30e865a71903c5c9
Jefferson <jsilvardg@outlook.com>
2022-07-11 13:42:42 -0300

    Add project files.

---
+++ FirstSiteMVC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using FirstSiteMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstSiteMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+            home.Nome = "Jefferson Rodrigues";
+            home.Email = "jsilvardg@outlook.com";
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
